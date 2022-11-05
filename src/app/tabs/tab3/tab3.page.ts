e3b99bdb219f4fe2f2227013344a4e2416e6eabe
Badredine Benfraj <badrbenfraj@mail.com>
2022-11-05 10:52:34 +0100

    feat(): add statistics page and profile page

--- src/app/tabs/tab3/tab3.page.ts
+++ src/app/tabs/tab3/tab3.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { filter, map } from 'rxjs/operators';
+import { FastingPerson } from 'src/app/core/model/fasting-person.model';
+import { FastingPersonService } from 'src/app/core/service/fasting-person.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  fastingPersonsList: FastingPerson[] = [];
 
-  constructor() {}
+  constructor(private fastingPersonService: FastingPersonService) {}
 
+  ngOnInit(): void {
+    this.getFastionsPersonsList();
+  }
+
+  getFastionsPersonsList() {
+    this.fastingPersonService
+      .getFastingPersons()
+      .pipe(
+        map((items) =>
+          items.filter(
+            (item: FastingPerson) =>
+              new Date(item.lastTakenMeal).setHours(0, 0, 0, 0) ===
+              new Date().setHours(0, 0, 0, 0)
+          )
+        )
+      )
+      .subscribe((items) => (this.fastingPersonsList = items));
+  }
+
+  getMealsNumber() {
+    let fastingPeople = 0;
+    for (const person of this.fastingPersonsList) {
+      fastingPeople += person.fasting;
+    }
+    return fastingPeople;
+  }
 }
